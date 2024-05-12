a38550b05bfa7d3044c8238b96eef22b7a67c175
Chandru-Ghub <chandruinfo455@gmail.com>
2024-05-12 21:33:08 +0530

    added

--- src/components/Frame2.js
+++ src/components/Frame2.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { MdDateRange } from "react-icons/md";
 const Frame2 = ({color,event,date,setDate,setEvent}) => {
 
   return (
@@ -7,7 +8,7 @@ const Frame2 = ({color,event,date,setDate,setEvent}) => {
             <input value={event} style={{color:!color?'#039d22':'#E92C2C'}} onChange={(e)=>setEvent(e.target.value)} type="text" placeholder='Enter event' />
         </div>
         <div className="cal">
-        <input value={date} onChange={(e)=>setDate(e.target.value)} type="text" placeholder='Enter Date and time' />
+        <MdDateRange style={{color:'#b5b5b5', marginLeft:'-15px',marginRight:'6px'}} /><input value={date} onChange={(e)=>setDate(e.target.value)} type="text" placeholder='Enter Date and time' />
         </div>
     </div>
   )
