d4fa446e8c0fa4790009f47807c39e3d61f2b623
Chandru-Ghub <chandruinfo455@gmail.com>
2024-05-12 21:11:31 +0530

    added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ function App() {
   return (
     <div className="App">
       <Frame/>
-      {/* <div>
-        <h1>welcome</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores maxime aliquid eaque quae natus laborum, voluptates laboriosam fuga tenetur corporis.</p>
-      </div> */}
     </div>
   );
 }
